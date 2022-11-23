9e3412c3ee97dd028fd9034703ea3f2c05acb8df
Sokolanov Matvey <msokolanov@gmail.com>
2022-11-23 12:26:35 +0700

    feat: add app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger("Bootstrap")
+
+const DEFAULT_PORT = 3000
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	
+	const configService = app.get(ConfigService)
+	const PORT = configService.get("PORT") || DEFAULT_PORT
+	
+	await app.listen(PORT, ()=> {
+		logger.log(`Server running at ${PORT}`) 
+	});
+
 }
 bootstrap();
